55b2f0547cc505d724fd514983f7021b3c1a8af1
Felipe@MBP <dataf4l@gmail.com>
2016-10-30 22:39:02 -0500

    faster\!

--- 2.js
+++ 2.js
@@ -1,12 +1,12 @@
 var desired_location = 0;
-var default_speed = 150; // px
+var default_speed = 250; // px
 var timer_speed = 25; // ms
 function scrollTo(desired_location){
 	var sw = window.screen.width;
-	if(Math.abs(document.body.scrollLeft - desired_location * sw) > (sw * 4)){
-		document.body.scrollLeft =  desired_location * sw;
-		return;
-	}
+	//if(Math.abs(document.body.scrollLeft - desired_location * sw) > (sw * 4)){
+	//	document.body.scrollLeft =  desired_location * sw;
+	//	return;
+	//}
 
 	var speed = Math.min(default_speed, Math.abs(document.body.scrollLeft - desired_location * sw));
 	if(document.body.scrollLeft > desired_location * sw) {
