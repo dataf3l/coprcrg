03011874ac27102131a527bc89f50a3f1b4aee9c
Felipe@MBP <dataf4l@gmail.com>
2016-10-30 23:05:14 -0500

    fix animation doesn't align with container

--- 2.js
+++ 2.js
@@ -1,8 +1,10 @@
 var desired_location = 0;
 var default_speed = 150; // px
-var timer_speed = 25; // ms
+var timer_speed = 50; // ms
+var the_timer = null;
 function scrollTo(desired_location){
-	var sw = window.screen.width;
+	//var sw = window.screen.width;
+	var sw = document.body.clientWidth;
 	//if(Math.abs(document.body.scrollLeft - desired_location * sw) > (sw * 4)){
 	//	document.body.scrollLeft =  desired_location * sw;
 	//	return;
@@ -10,13 +12,18 @@ function scrollTo(desired_location){
 
 	var speed = Math.min(default_speed, Math.abs(document.body.scrollLeft - desired_location * sw));
 	if(document.body.scrollLeft > desired_location * sw) {
-		document.body.scrollLeft-= speed;
+		document.body.scrollLeft= (parseFloat(document.body.scrollLeft) - speed) ;
 	}
 	if(document.body.scrollLeft < desired_location * sw) {
-		document.body.scrollLeft+= speed;
+		document.body.scrollLeft= (speed + parseFloat(document.body.scrollLeft)) ;
 	}
+	if(Math.abs(document.body.scrollLeft - desired_location * sw)<2) {
+		clearTimeout(the_timer);
+	}
+	
 	if(document.body.scrollLeft != desired_location * sw){
-		setTimeout(function(){scrollTo(desired_location)}, timer_speed);
+		clearTimeout(the_timer);
+		the_timer = setTimeout(function(){scrollTo(desired_location)}, timer_speed);
 	}
 	//document.body.scrollLeft = ( * pos);
 }
